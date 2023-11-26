2fea63c21c26dab335ccaef56ac8ce2ddccaa91f
hkoroor <hkmotlagh@gmail.com>
2023-11-26 13:57:11 +0330

    feat(dev-script): Developing counter effect for coursesTime & coursesCont & clintsCount

--- js/index.js
+++ js/index.js
@@ -1,15 +1,27 @@
 const $ = document;
+// Dom Selection
 const landingTitle = $.querySelector(".landing__title");
+const coursesTime = $.querySelector("#couses-time");
+const coursesCount = $.querySelector("#courses-count");
+const clintsCount = $.querySelector("#clints-count");
 
-console.log(landingTitle);
+// Default Variables
+let maxCoursesTime = 3_320;
+let maxCoursesCount = 40;
+let maxClintsCount = 3_071;
 
 window.addEventListener("load", () => {
   let landingText = "ما به هر قیمتی دوره آموزشی تولید نمی کنیم !";
   const textIndex = 0;
 
   typeWriter(landingText, textIndex);
+
+  makeCounter(coursesTime, maxCoursesTime, 0, 1);
+  makeCounter(coursesCount, maxCoursesCount, 0, 100);
+  makeCounter(clintsCount, maxClintsCount, 0, 1);
 });
 
+// TypeWriter Func
 const typeWriter = (text, index) => {
   if (index < text.length) {
     landingTitle.innerHTML += text[index];
@@ -19,4 +31,16 @@ const typeWriter = (text, index) => {
       typeWriter(text, index);
     }, 100);
   }
+};
+
+// Interval Func
+const makeCounter = (elem, maxCount, counter, delay) => {
+  const interval = setInterval(() => {
+    counter++;
+    elem.innerHTML = counter;
+
+    if (counter === maxCount) {
+      clearInterval(interval);
+    }
+  }, delay);
 };
